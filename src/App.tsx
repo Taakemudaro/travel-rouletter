9dd21974285ced653c2916a7f2070420bb88e6be
Yuki <yuki1027amrboowy@gmail.com>
2024-12-31 12:40:14 +0900

    TOPページ全体の作成、AtomicDesignの部品作成 #2

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,16 @@ import { BrowserRouter } from "react-router-dom";
 import { Router } from "./router/Router";
 import { createGlobalStyle, ThemeProvider } from "styled-components";
 import theme from "./theme/theme";
-import Div100vh from "react-div-100vh";
 
 function App() {
   return (
     <div className="App">
-      <Div100vh>
-        <ThemeProvider theme={theme}>
-          <SGlobalStyle />
-          <BrowserRouter>
-            <Router />
-          </BrowserRouter>
-        </ThemeProvider>
-      </Div100vh>
+      <ThemeProvider theme={theme}>
+        <SGlobalStyle />
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ThemeProvider>
     </div>
   );
 }
@@ -26,6 +23,6 @@ export default App;
 const SGlobalStyle = createGlobalStyle`
   body {
     background-color: ${(props) => props.theme.styles.body.backgroundColor};
-    color: ;
+    color: ${(props) => props.theme.styles.body.color};
   }
 `;
